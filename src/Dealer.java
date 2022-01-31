b21eb709290dcdd9022b2982c9fa4bce8f3757f8
jrodriq3 <86337121+jrodriq3@users.noreply.github.com>
2022-01-31 14:19:23 -0500

    commit message

--- src/Dealer.java
+++ src/Dealer.java
@@ -1,8 +1,11 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Dealer {
+    public int total = 0;
+    public ArrayList<String> dealerCards = new ArrayList<String>();
     public static ArrayList<String> deckCards = new ArrayList<String>();
     public static ArrayList<String> referenceCards = new ArrayList<String>();
     public static ArrayList<Integer> referenceValues = new ArrayList<Integer>();
@@ -34,14 +37,50 @@ public class Dealer {
 
         }
     }
-
     // used to add a single card to ArrayList object
     public void addCard(ArrayList<String> x) {
         Random random = new Random();
-        int ranNumb = random.nextInt(52);
+        int ranNumb = random.nextInt(deckCards.size());
         String tempVar = deckCards.get(ranNumb);
         deckCards.remove(ranNumb);
         x.add(tempVar);
+    }
+    public void dealersTurn(Dealer o) {
+        System.out.println("Dealers cards are: " + o.dealerCards);
+        o.calculateTotal(null, o);
+        System.out.println("Dealers total: " + o.total);
+        while (o.total < 16) {
+            o.addCard(o.dealerCards);
+            o.calculateTotal(null, o);
+            System.out.println("Dealers cards: " + o.dealerCards);
+            System.out.println("Dealers total: " + o.total);
+        }
+    }
+    public void promptUserAndHit(Player O) {
+        if (O.total == 21) {
+            System.out.println("Congrats, you got 21!");
+            O.isDone = true;
+        }
+        else {
+            while (O.total < 21 && O.isDone == false && O.isBusted == false) {
+                System.out.println("Enter 1 for another card. or 2 to stay.");
+                Scanner scanner = new Scanner(System.in);
+                int answer = scanner.nextInt();
+                if (answer == 1) {
+                    addCard(O.playerCards);
+                    System.out.println(O.playerCards);
+                    calculateTotal(O, null);
+                    System.out.println(O.total);
+                    if (O.total > 21) {
+                        System.out.println("Sorry, you busted.");
+                    }
+                }
+                else {
+                    System.out.println("Your turn is over.");
+                    break;
+                }
+            }
+        }
     }
     // uses addCard method twice in this method to simulate the start of game
     public void dealStart(ArrayList<String> x) {
@@ -50,17 +89,46 @@ public class Dealer {
         dealer.addCard(x);
     }
     // method used to calculate total given an arraylist of cards as the parameter
-    public void calculateTotal(ArrayList<String> hand, Player O) {
-        O.total = 0;
-        for (String item:hand) {
-            for (int i = 0; i < referenceCards.size(); i++) {
-                if (item == referenceCards.get(i)) {
-                    O.total = O.total + referenceValues.get(i);
+    public void calculateTotal(Player Pobj, Dealer Dobj) {
+        if (Dobj == null) {
+            Pobj.total = 0;
+            for (String item:Pobj.playerCards) {
+                for (int i = 0; i < referenceCards.size(); i++) {
+                    if (item == referenceCards.get(i)) {
+                        Pobj.total = Pobj.total + referenceValues.get(i);
+                    }
                 }
-
+            }
+        }
+        else {
+            Dobj.total = 0;
+            for (String item:Dobj.dealerCards) {
+                for (int i = 0; i < referenceCards.size(); i++) {
+                    if (item == referenceCards.get(i)) {
+                        Dobj.total = Dobj.total + referenceValues.get(i);
+                    }
+                }
             }
         }
         // this part of the code is re-adjusting the total if over 21 and has ace in hand.
-        System.out.println(O.total);
+        if (Dobj == null) {
+            if (Pobj.total > 21) {
+                for (String item:Pobj.playerCards) {
+                    if (item.contains("A") && Pobj.total > 21) {
+                        Pobj.total -= 10;
+                    }
+                }
+            }
+        }
+        else {
+            if (Dobj.total > 21) {
+                for (String item:Dobj.dealerCards) {
+                    if (item.contains("A") && Dobj.total > 21) {
+                        Dobj.total -= 10;
+                    }
+                }
+            }
+        }
+        //System.out.println(O.total);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,26 @@ public class Main {
         myDealer.populateCardArrs();
         Player playerJason = new Player();
         myDealer.dealStart(playerJason.playerCards);
-        System.out.println(playerJason.playerCards);
-        myDealer.calculateTotal(playerJason.playerCards, playerJason);
+        System.out.println("Your cards: " + playerJason.playerCards);
+        myDealer.dealStart(myDealer.dealerCards);
+        for (int i = 1; i < myDealer.dealerCards.size(); i++) {
+            System.out.println("Dealers cards: " + myDealer.dealerCards.get(i));
+        }
+        myDealer.calculateTotal(playerJason, null);
+        System.out.println("Your total is: " + playerJason.total);
+        myDealer.promptUserAndHit(playerJason);
+        if (playerJason.total > 21) {
+            playerJason.isBusted = true;
+        }
+        if (playerJason.isBusted == false) {
+            myDealer.dealersTurn(myDealer);
+        }
+        else {
+            System.out.println("Sorry you busted the dealer won.");
+        }
+        if (playerJason.total > myDealer.total && playerJason.total < 22) {
+            System.out.println("You win!");
+        }
+        }
     }
-}
+
